191f722a70b8aed5700b81c02be4480bbb7e282b
ggolaeff <96593755+ggolaeff@users.noreply.github.com>
2022-07-06 11:32:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
 
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), primary_key=False)
+    email = db.Column(db.String(100), primary_key=False)
+    role = db.Column(db.Integer, primary_key=False)
+    password = db.Column(db.String(100), primary_key=False)
+
+    def __repr__(self):
+        return '<Users %r>' % self.id
 
 @app.route('/signup')
 def sign():
