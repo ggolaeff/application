b10bdf02afcb409414f21640c90c07efff2e7463
ggolaeff <96593755+ggolaeff@users.noreply.github.com>
2022-07-07 11:19:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,29 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect
 
+
+from add_user import *
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-db = SQLAlchemy(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'real secret key'
+db.init_app(app)
 
 
-class Users(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), primary_key=False)
-    email = db.Column(db.String(100), primary_key=False)
-    role = db.Column(db.Integer, primary_key=False)
-    password = db.Column(db.String(100), primary_key=False)
-
-    def __repr__(self):
-        return '<Users %r>' % self.id
-
-@app.route('/signup')
+@app.route('/signup', methods=['POST', 'GET'])
 def sign():
-    return render_template("sign.html")
+    id = request.form.get('login')
+    password = request.form.get('password')
+
+    if check_auth(id, password):
+        return redirect('tasks')
+    else:
+        return render_template("sign.html")
+
+
+@app.route('/tasks')
+def tasks():
+    return render_template("tasks.html")
 
 
 if __name__ == "__main__":
