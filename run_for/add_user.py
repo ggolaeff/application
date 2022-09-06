aae79b5ef2a5917530a5601f30eaf6f073ba42c8
DarliDarliNgton <103196598+DarliDarliNgton@users.noreply.github.com>
2022-09-06 19:13:51 +0300

    Add files via upload

---
+++ run_for/add_user.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from flask_login import UserMixin, LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+from werkzeug.security import check_password_hash, generate_password_hash
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///application.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'real secret key'
+db = SQLAlchemy(app)
+manager = LoginManager(app)
+
+
+class Users(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), primary_key=False)
+    contact = db.Column(db.String(100), primary_key=False)
+    role = db.Column(db.Integer, primary_key=False)
+    password = db.Column(db.String(100), primary_key=False)
+
+    def __repr__(self):
+        return '<Users %r>' % self.id
+
+
+@manager.user_loader
+def load_user(user_id):
+    return Users.query.get(user_id)
+
+
+def check_auth(login, password):
+    check = Users.query.filter(Users.id == login).all()
+    if not check:
+        return False
+    else:
+        if check_password_hash(check[0].password, password):
+            return True
+        else:
+            return False
+
+
+def create_new_password(user, password):
+    user.password = generate_password_hash(password)
+    db.session.commit()
---
+++ run_for/task_edit.py
@@ -0,0 +1,19 @@
+from add_user import db
+
+
+class Tasks(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name_task = db.Column(db.String(100), primary_key=False)
+    from_user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    to_user_id = db.Column(db.Integer, primary_key=False)
+    text_task = db.Column(db.Text, primary_key=False)
+    files = db.Column(db.Text, primary_key=False)
+    begin_date = db.Column(db.DateTime, primary_key=False)
+    end_date = db.Column(db.DateTime, primary_key=False)
+    status = db.Column(db.String(100), primary_key=False)
+    id_ping = db.Column(db.Integer, primary_key=False)
+    elevations = db.Column(db.Integer, primary_key=False)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
